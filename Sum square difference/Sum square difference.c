2bfa2f5732f615d46822e01e0776f454e0139b7e
CHINNAPAT SIRIYAJAI <63010231@KMITL.AC.TH>
2020-08-14 14:58:58 +0700

    v1

---
+++ Sum square difference/Sum square difference.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+	long num, x = 0, y = 0, i, j;
+	for (i = 1; i <= 100; i++) {
+		x = x + pow(i, 2);
+	}
+	for (j = 1; j <= 100; j++) {
+		y = y + i;
+	}
+	num = pow(y, 2) - x;
+	printf("%d ", num);
+}
